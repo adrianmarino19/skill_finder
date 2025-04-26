391947a473810b7f3eab121e71a264c622f74342
am <adrianmarino10@gmail.com>
2025-04-26 21:32:27 +0200

    removing debug info

--- backend.py
+++ backend.py
@@ -318,7 +318,7 @@ def run_pipeline(keywords: str, location: str, pages_to_scrape: int,
     jobs = scrape_jobs_with_descriptions(keywords, location, pages_to_scrape, headers,
                                          experience_level, remote, date_posted, benefits,
                                          easy_apply, sortby)
-    st.write(f"🔍 Scraped {len(jobs)} jobs") # debugging
+    # st.write(f"🔍 Scraped {len(jobs)} jobs") # debugging
     if not jobs:
         return None, None
 
